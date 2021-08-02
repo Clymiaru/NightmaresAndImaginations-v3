e5462b2cc91772334623adb0cd365d1eef2b832f
Clymiaru <lscirno@gmail.com>
2021-08-02 14:26:25 +0800

    Imported ExtendedSaveSystem

---
+++ NightmaresAndImaginations/Assets/Imported/ExtendableSaveSystem/Core/ISavableComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NGS.ExtendableSaveSystem
+{
+    public interface ISavableComponent
+    {
+        int UniqueID { get; }
+        int ExecutionOrder { get; }
+
+        ComponentData Serialize();
+
+        void Deserialize(ComponentData data);
+    }
+}
