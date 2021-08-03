59aabdbf996ad210a867738da5236a584b62eaca
DESKTOP-1HOV35S\aljon <>
2021-08-04 02:22:26 +0800

    edited movement, added jump (3)

--- NightmaresAndImaginations/Assets/Scripts/PlayerController/PlayerMovement.cs
+++ NightmaresAndImaginations/Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
+    public float jumpForce = 15f;
     public Rigidbody2D rb;
     public Animator animator;
 
-    Vector2 movement;
+    float movement;
+    Vector2 wew;
 
-    void Update()
+    int maxJumps = 3;
+    int jumps = 0;
+
+
+    private void Start()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        this.jumps = this.maxJumps;
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        rb = this.GetComponent<Rigidbody2D>();
+        if (rb == null)
+            Debug.LogError("Missing Rigidbody2D in 'PlayerMovement' script!");
     }
 
-    private void FixedUpdate()
+
+    private void Update()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed* Time.fixedDeltaTime);
+        this.movement = Input.GetAxisRaw("Horizontal");
+
+        animator.SetFloat("Horizontal", this.movement);
+        animator.SetFloat("Speed", movement * movement);
+
+        this.transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * this.moveSpeed;
+
+        if (Input.GetButtonDown("Jump") && this.jumps > 0)
+        {
+            this.rb.velocity = new Vector2(this.rb.velocity.x, 0);
+            this.rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            this.jumps--;
+        }
+        else if (this.rb.velocity.y == 0.0f)
+            this.jumps = this.maxJumps;
     }
+
+  
 }
