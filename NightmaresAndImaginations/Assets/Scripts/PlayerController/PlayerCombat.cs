20bbbcf660803af5f79698b2efdd412647dbb7f8
DESKTOP-1HOV35S\aljon <>
2021-08-05 02:54:46 +0800

    Improved overall movement, jumping, and added simple attack

--- NightmaresAndImaginations/Assets/Scripts/PlayerController/PlayerCombat.cs
+++ NightmaresAndImaginations/Assets/Scripts/PlayerController/PlayerCombat.cs
@@ -4,23 +4,74 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    public Animator animator;
+    private PlayerMovement movementRef;
+
+    public float attackRate = 5.0f;
+    private float nextAttackTime = 0.0f;
+
+    float attackRange = 0.5f;
+    public Transform attackPointRight;
+    public Transform attackPointLeft;
+    public LayerMask enemyLayers;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.movementRef = this.GetComponent<PlayerMovement>();
+        if (this.movementRef == null)
+            Debug.LogError("Script PlayerCombat: no reference to PlayerMovement component!");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Time.time >=  this.nextAttackTime)
         {
-            Attack();
+            if (Input.GetMouseButtonDown(0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / this.attackRate;
+            }    
         }
+
+       if(this.animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+       {
+            this.movementRef.SetMovement(false);
+       }
+       else
+       {
+            this.movementRef.SetMovement(true);
+        }
+
     }
 
     private void Attack()
     {
+        animator.SetFloat("AttackDirection", this.movementRef.GetAttackDirection());
+        animator.SetTrigger("Attack");
+
+        Collider2D[] hitEnemies = null; ;
+        
+        if(this.movementRef.GetAttackDirection() > 0)
+            hitEnemies = Physics2D.OverlapCircleAll(this.attackPointRight.position, this.attackRange, this.enemyLayers);
+        else if (this.movementRef.GetAttackDirection() < 0)
+            hitEnemies = Physics2D.OverlapCircleAll(this.attackPointLeft.position, this.attackRange, this.enemyLayers);
+
+        for(int i = 0; i < hitEnemies.Length; i++)
+        {
+            Debug.Log("Hit enemy " + hitEnemies[i].name);
+            //do damage calculation here
+        }
 
     }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(this.attackPointRight.position, this.attackRange);
+        Gizmos.DrawWireSphere(this.attackPointLeft.position, this.attackRange);
+    }
+
 }
--- NightmaresAndImaginations/Assets/Scripts/PlayerController/PlayerMovement.cs
+++ NightmaresAndImaginations/Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
@@ -9,6 +11,12 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
 
+    public Transform headPoint;
+    public LayerMask groundLayers;
+    Collider2D[] hitHeadGround;
+
+    float lastMove = 0.0f;
+    float colliderRadius = 0.9f;
     float movement;
     Vector2 wew;
 
@@ -16,10 +24,12 @@ public class PlayerMovement : MonoBehaviour
     int jumps = 0;
 
 
+    bool canMove = true;
+
+
     private void Start()
     {
         this.jumps = this.maxJumps;
-
         rb = this.GetComponent<Rigidbody2D>();
         if (rb == null)
             Debug.LogError("Missing Rigidbody2D in 'PlayerMovement' script!");
@@ -28,13 +38,38 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        this.movement = Input.GetAxisRaw("Horizontal");
+        this.MovePlayer();
+        this.Jump();
+       
+    }
+
+  
+    private void MovePlayer()
+    {
+        if(this.canMove)
+        {
+            this.movement = Input.GetAxisRaw("Horizontal");
+
+            if (this.movement != 0)
+                this.lastMove = this.movement;
+        }
+        else if (this.rb.velocity.y == 0.0)
+        {
+            this.movement = 0.0f;
+        }
+
+       
+        this.transform.position += new Vector3(this.movement, 0, 0) * Time.deltaTime * this.moveSpeed;
+
+
 
         animator.SetFloat("Horizontal", this.movement);
         animator.SetFloat("Speed", movement * movement);
+    }
 
-        this.transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * this.moveSpeed;
-
+    private void Jump()
+    {
+        //Jumping
         if (Input.GetButtonDown("Jump") && this.jumps > 0)
         {
             this.rb.velocity = new Vector2(this.rb.velocity.x, 0);
@@ -43,7 +78,42 @@ public class PlayerMovement : MonoBehaviour
         }
         else if (this.rb.velocity.y == 0.0f)
             this.jumps = this.maxJumps;
+
+        //Make player go through ground when jumping
+        if (this.rb.velocity.y > 0.0)
+        {
+            if(this.hitHeadGround != null)
+            {
+                for (int i = 0; i < this.hitHeadGround.Length; i++)
+                {
+                    this.hitHeadGround[i].GetComponent<BoxCollider2D>().enabled = true;
+                }
+            }
+            Collider2D[] temp = Physics2D.OverlapCircleAll(this.headPoint.position, this.colliderRadius, this.groundLayers);
+            if(temp != null)
+                this.hitHeadGround = temp;
+
+            for (int i = 0; i < this.hitHeadGround.Length; i++)
+            {
+                this.hitHeadGround[i].GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
+       
     }
 
-  
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(this.headPoint.position, this.colliderRadius);
+    }
+
+
+    public float GetAttackDirection()
+    {
+        return this.lastMove;
+    }
+
+    public void SetMovement(bool move)
+    {
+        this.canMove = move;
+    }
 }
