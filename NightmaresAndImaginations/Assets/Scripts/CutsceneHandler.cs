1cea4255065c02f12fa32a74a9821261cfdf9a46
Clymiaru <lscirno@gmail.com>
2021-09-16 20:13:43 +0800

    Initial implementation of dialogue and imported PixelOperator font

--- NightmaresAndImaginations/Assets/Scripts/CutsceneHandler.cs
+++ NightmaresAndImaginations/Assets/Scripts/CutsceneHandler.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace TDS
@@ -13,80 +10,40 @@ namespace TDS
         Ending
     }
 
-    [Serializable]
-    public struct CutsceneMeta
-    {
-        public string SceneName;
-        public AnimationClip Cutscene;
-    }
-    
     public class CutsceneHandler : MonoBehaviour
     {
-        [SerializeField] private List<CutsceneMeta> CutsceneInfoList;
-        
-        [SerializeField] private UnityEvent OnEndCutscene;
+        [SerializeField] private Animator CutsceneAnimator;
+        [Header("Fade In/Out Info")] [SerializeField]
+        private Image FadePanel;
 
-        [SerializeField] private string CurrentSceneName;
+        [Header("Fade In Info")] [SerializeField]
+        private float FadeInDuration;
 
-        [Header("Fade In/Out Info")]
-        [SerializeField] private Image FadePanel;
-
-        [Header("Fade In Info")] 
-        [SerializeField] private float FadeInDuration;
-        
-        [Header("Fade Out Info")]
-        [SerializeField] private float FadeOutDuration;
+        [Header("Fade Out Info")] [SerializeField]
+        private float FadeOutDuration;
 
         private Sequence enterCutsceneTween;
         private Sequence exitCutsceneTween;
-        
-        private Animator cutsceneAnimator;
-        private AnimatorOverrideController cutsceneAnimatorOverride;
-        
-        private const string BaseCutsceneAnimTag = "BaseCutsceneAnim";
 
         private void Awake()
         {
             DOTween.Init();
-            
-            // Idea is that when we load a save file, we obtain current chapter information.
-            // Based on that current chapter information, we load the appropriate animation clip
-            // Since, the cutscene will only be played at the beginning of each level.
-            // Except at the last level where it will also be played at the end.
-            
-            // ASK: How will the cutscene be played?
-            // Example: 
-            // There will be a fade in transition at the beginning
-            // Main cutscene will play
-            // Then, fade out transition at the end (which will also occur when skipped)
-
-            cutsceneAnimator = gameObject.GetComponent<Animator>();
-            cutsceneAnimatorOverride = new AnimatorOverrideController(cutsceneAnimator.runtimeAnimatorController);
-            cutsceneAnimator.runtimeAnimatorController = cutsceneAnimatorOverride;
         }
 
         private void Start()
         {
-            // TODO: Add get chapter information from the save file
-            InitializeCutscene();
-
             enterCutsceneTween = DOTween.Sequence();
             exitCutsceneTween = DOTween.Sequence();
-            
+
             InitializeTweenAnimation();
-            
+
             PlayCutscene();
         }
-        
+
         private void OnDestroy()
         {
             enterCutsceneTween.Kill();
             exitCutsceneTween.Kill();
-        }
-
-        private void InitializeCutscene()
-        {
-            cutsceneAnimatorOverride[BaseCutsceneAnimTag] = GetCutsceneOfCurrentSceneName();
         }
 
         private void InitializeTweenAnimation()
@@ -96,13 +53,13 @@ namespace TDS
             exitCutsceneTween.Append(FadePanel.DOFade(255, FadeOutDuration)
                                               .OnComplete(ExecuteOnEndCutsceneEvent));
         }
-        
+
         public void PlayCutscene()
         {
             Debug.Log("Play Cutscene!");
-            
+
             // Forced to take in account fade in transition?
-            cutsceneAnimator.SetTrigger(CutsceneTrigger.Playing.ToString());
+            CutsceneAnimator.SetTrigger(CutsceneTrigger.Playing.ToString());
             ExecuteFadeInTransition();
         }
 
@@ -115,13 +72,12 @@ namespace TDS
         public void EndCutscene()
         {
             Debug.Log("End Cutscene!");
-            cutsceneAnimator.SetTrigger(CutsceneTrigger.Ending.ToString());
+            CutsceneAnimator.SetTrigger(CutsceneTrigger.Ending.ToString());
             ExecuteFadeOutTransition();
         }
-        
+
         private void ExecuteOnEndCutsceneEvent()
         {
-            OnEndCutscene?.Invoke();
         }
 
         private void ExecuteFadeInTransition()
@@ -135,22 +91,6 @@ namespace TDS
             exitCutsceneTween.Restart();
             exitCutsceneTween.Play();
         }
-
-        private AnimationClip GetCutsceneOfCurrentSceneName()
-        {
-            for (var i = 0; i < CutsceneInfoList.Count; i++)
-            {
-                if (CutsceneInfoList[i].SceneName != CurrentSceneName)
-                {
-                    continue;
-                }
-                
-                return CutsceneInfoList[i].Cutscene;
-            }
-
-            return null;
-        }
-        
-    }    
+    }
 }
 
